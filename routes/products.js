8d791546dfcafe90a6c68299bf81a2b3d40a7ce8
Pr4v33n23 <praveen.jaisankar@gmail.com>
2020-08-18 03:17:54 +0530

    All APIs done, tweaks and test pending

--- routes/products.js
+++ routes/products.js
@@ -24,36 +24,101 @@ router.get('/', async(req, res) => {
 });
 
 //Get product by given ID
-router.get('/:id', async( req, res) => {
+router.get('/:productId', async( req, res) => {
     const data = await fs.readFile(productPathFile);
     const products = JSON.parse(data);
-    const product = products.find( p => p.productId ===  parseInt(req.params.id));
+    const product = products.find( p => p.productId ===  parseInt(req.params.productId));
     if(!product) return res.status(404).json({
         "statusCode": 404,
         "status": "failure",
-        "message": `product with Id:${req.params.id} does not exist`,
+        "message": `product with ID:${req.params.productId} does not exist`,
         "data": []
     });
 
     res.status(200).json({
         "statusCode": 200,
-        "status": "sucess",
+        "status": "success",
         "message": "product data found",
         "data": [product]
     });
 });
 
-//router.post()
-//router.put()
-
-router.delete('/:id', async(req, res) => {
+router.post('/', async (req, res) => {
     const data = await fs.readFile(productPathFile);
     const products = JSON.parse(data);
-    const product = products.find( p => p.productId === parseInt(req.params.id))
+    const newProduct = {
+    "productId": req.body.productId,
+    "productName": req.body.productName,
+    "productCode": req.body.productcode,
+    "releaseDate": req.body.releaseDate, 
+    "description": req.body.description,
+    "price": req.body.price,
+    "starRating": req.body.starRating,
+    "imageUrl": req.body.imageUrl 
+    }
+
+    const product = products.find( p => p.productId === parseInt( req.body.productId))
+
+    if(product) return res.status(400).json({
+        "statusCode": 400,
+        "status": "failure",
+        "message": `product with ID:${req.body.productId} already exists. Please provide different ID`,
+        "data": []
+    });
+
+    products.push(newProduct);
+
+    fs.writeFile(productPathFile, JSON.stringify(products));
+
+    res.status(201).json({
+            "statusCode": 201,
+            "status": "success",
+            "message": "product data added successfully",
+            "data": [newProduct]
+    });
+});
+
+
+router.put('/:productId', async(req, res) => {
+
+    const data = await fs.readFile(productPathFile);
+    const products = JSON.parse(data);
+    const product = products.find( p => p.productId === parseInt(req.params.productId));
+
     if(!product) return res.status(404).json({
         "statusCode": 404,
         "status": "failure",
-        "message": `product with Id:${req.params.id} does not exist`,
+        "message": `product with ID:${req.params.productId} does not exist`,
+        "data": []
+    });
+
+    product.productName = req.body.productName;
+    product.productCode  = req.body.productcode;
+    product.releaseDate  = req.body.releaseDate;
+    product.description =  req.body.description;
+    product.price =  req.body.price;
+    product.starRating =  req.body.starRating;
+    product.imageUrl =  req.body.imageUrl;
+    
+    fs.writeFile(productPathFile, JSON.stringify(products));
+
+    res.status(201).json({
+        "statusCode": 201,
+        "status": "success",
+        "message": "product data added successfully",
+        "data": [product]
+    });
+});
+
+//Delete product by given ID
+router.delete('/:productId', async(req, res) => {
+    const data = await fs.readFile(productPathFile);
+    const products = JSON.parse(data);
+    const product = products.find( p => p.productId === parseInt(req.params.productId));
+    if(!product) return res.status(404).json({
+        "statusCode": 404,
+        "status": "failure",
+        "message": `product with ID:${req.params.productId} does not exist`,
         "data": []
     });
 
